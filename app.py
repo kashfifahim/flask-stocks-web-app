65094ae2cc15a39cca9da9b2068a8356f32639fb
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-12-21 10:03:28 -0500

    end of fixing errors

--- app.py
+++ app.py
@@ -37,21 +37,18 @@ def is_cache_valid(filename):
 
 
 def read_cache(filename):
-    try:
-        with open(filename, 'r') as file:
-            return json.load(file)
-    except Exception as e:
-        print(f"Error reading cache from file {filename}: {e}")
-        return None
-    
+    with open(filename, 'r') as file:
+        return json.load(file)
+
 
 def write_cache(filename, data):
-    try:
-        os.makedirs(CACHE_DIR, exist_ok=True)
-        with open(filename, 'w') as file:
-            json.dump(data, file)
-    except Exception as e:
-        print(f"Error writing cache to file {filename}: {e}")
+    cache_data = {
+        "price": data,
+        "timestamp": time.time()
+    }
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    with open(filename, 'w') as file:
+        json.dump(cache_data, file)
 
 
 stocks = {
@@ -65,51 +62,38 @@ stocks = {
 
 # Function to get stock prices
 def get_stock_prices(symbol):
-    try: 
-        # Caching logic here
-        filename = cache_filename(symbol)
-        if is_cache_valid(filename):
-            return read_cache(filename)["price"]
-        
-        response = requests.get(API_URL, {
-            "function": "GLOBAL_QUOTE",
-            "symbol": symbol,
-            "apikey": API_KEY
-        })
-        data = response.json()
-        print(data)
-        price = float(data["Global Quote"]["05. price"])
+    filename = cache_filename(symbol)
+    if is_cache_valid(filename):
+        cache_data = read_cache(filename)
+        return cache_data["price"], cache_data.get("timestamp")
 
-        # Write new data to cache
-        write_cache(filename, {"price": price})
-
-        return price
-    except requests.exceptions.RequestException as e:
-        print(f"Request error for symbol {symbol}: {e}")
-        return None
-    except ValueError as e:
-        print(f"Value error: could not convert data to float for symbol {symbol}: {e}")
-        return None
+    try:
+        # Make API call...
+        # On successful API response:
+        write_cache(filename, price)
+        return price, time.time()
     except Exception as e:
-        print(f"An unexpected error occurred for symbol {symbol}: {e}")
-        return None
+        # If API call fails, return the last cached price if available
+        if os.path.exists(filename):
+            cache_data = read_cache(filename)
+            return cache_data["price"], cache_data.get("timestamp")
+        else:
+            return None, None
+
 
 @app.route('/')
 def index():
     total_value = 0
     prices = {}
-    errors = {}
+    timestamps = {}
     for stock, quantity in stocks.items():
-        price = get_stock_prices(stock)
+        price, timestamp = get_stock_prices(stock)
         if price is not None:
             total_value += price * quantity
-            prices[stock] = price
-        else:
-            # Handle the scenario where price is None
-            errors[stock] = "Price data unavailable"
-            prices[stock] = "N/A"  # You can set a placeholder like "N/A" or 0
+        prices[stock] = price if price is not None else "N/A"
+        timestamps[stock] = timestamp
 
-    return render_template('index.html', prices=prices, total=total_value, stocks=stocks, errors=errors)
+    return render_template('index.html', prices=prices, total=total_value, stocks=stocks, timestamps=timestamps)
 
 
 if __name__ == '__main__':
