ec47affd542624acd52570140fd23921cffadd21
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-12-21 09:26:38 -0500

    update code with try-except blocks to handle errors

--- app.py
+++ app.py
@@ -13,30 +13,45 @@ API_KEY = "Z26IND1A7LZTHK7V"
 
 # Caching
 CACHE_DIR = "cache"
-CACHE_DURATION = 600
+CACHE_DURATION = 3600
 
 
 def cache_filename(symbol):
-    return os.path.join(CACHE_DIR, f"{symbol}.json")
+    try:
+        return os.path.join(CACHE_DIR, f"{symbol}.json")
+    except Exception as e:
+        print(f"Error generating cache filename for symbol {symbol}: {e}")
+        return None
 
 
 def is_cache_valid(filename):
-    if not os.path.exists(filename):
+    try:
+        if not os.path.exists(filename):
+            return False
+        if time.time() - os.path.getmtime(filename) > CACHE_DURATION:
+            return False
+        return True
+    except Exception as e:
+        print(f"Error checking cache validity for file {filename}: {e}")
         return False
-    if time.time() - os.path.getmtime(filename) > CACHE_DURATION:
-        return False
-    return True
 
 
 def read_cache(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"Error reading cache from file {filename}: {e}")
+        return None
     
 
 def write_cache(filename, data):
-    os.makedirs(CACHE_DIR, exist_ok=True)
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+    try:
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+    except Exception as e:
+        print(f"Error writing cache to file {filename}: {e}")
 
 
 stocks = {
@@ -50,25 +65,34 @@ stocks = {
 
 # Function to get stock prices
 def get_stock_prices(symbol):
-    # Caching logic here
-    filename = cache_filename(symbol)
-    if is_cache_valid(filename):
-        return read_cache(filename)["price"]
-    
-    response = requests.get(API_URL, {
-        "function": "GLOBAL_QUOTE",
-        "symbol": symbol,
-        "apikey": API_KEY
-    })
-    data = response.json()
-    print(data)
-    price = float(data["Global Quote"]["05. price"])
+    try: 
+        # Caching logic here
+        filename = cache_filename(symbol)
+        if is_cache_valid(filename):
+            return read_cache(filename)["price"]
+        
+        response = requests.get(API_URL, {
+            "function": "GLOBAL_QUOTE",
+            "symbol": symbol,
+            "apikey": API_KEY
+        })
+        data = response.json()
+        print(data)
+        price = float(data["Global Quote"]["05. price"])
 
-    # Write new data to cache
-    write_cache(filename, {"price": price})
+        # Write new data to cache
+        write_cache(filename, {"price": price})
 
-    return price
-
+        return price
+    except requests.exceptions.RequestException as e:
+        print(f"Request error for symbol {symbol}: {e}")
+        return None
+    except ValueError as e:
+        print(f"Value error: could not convert data to float for symbol {symbol}: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred for symbol {symbol}: {e}")
+        return None
 
 @app.route('/')
 def index():
